150b3d3db850a09ad9ddde531bf5696954131ddf
KieVitz <s217442@sggw.edu.pl>
2024-03-26 23:05:58 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string fileName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnToggleMaximized_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void menuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    fileName = openFileDialog.FileName;
+                    string text = File.ReadAllText(fileName);
+                    txtBox.Text = text;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error reading the file: " + ex.Message);
+                }
+            }
+        }
+
+        private void menuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (fileName != null)
+            {
+                try
+                {
+                    File.WriteAllText(fileName, txtBox.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving the file: " + ex.Message);
+                }
+            }
+            else
+            {
+                menuSaveAs_Click(sender, e);
+            }
+        }
+
+        private void menuSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, txtBox.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving the file: " + ex.Message);
+                }
+            }
+        }
+    }
+}
